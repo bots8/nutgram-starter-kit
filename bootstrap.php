40465d343091463ae3714cfff69e2ab4690cca96
Fikri <fikrizhanking@gmail.com>
2024-01-27 19:20:05 +0700

    Create bootstrap file

---
+++ bootstrap.php
@@ -0,0 +1,33 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+use SergiX44\Nutgram\Nutgram;
+use SergiX44\Nutgram\RunningMode\Webhook;
+use SergiX44\Nutgram\RunningMode\Polling;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\Psr16Cache;
+use SergiX44\Nutgram\Configuration;
+
+
+$psr6Cache = new FilesystemAdapter();
+$psr16Cache = new Psr16Cache($psr6Cache);
+
+$bot = new Nutgram($_ENV['BOT_TOKEN'], [
+    'cache' => $psr16Cache
+]);
+
+require __DIR__.'/handler.php';
+
+// Running mode
+if($_ENV['ENV'] == 'development') {
+	$bot->setRunningMode(Polling::class);
+} else {
+	$bot->setRunningMode(Webhook::class);
+}
+
+return $bot;
--- index.php
+++ index.php
@@ -1,34 +1,6 @@
 <?php
 
-require 'vendor/autoload.php';
+$app = require_once __DIR__.'/bootstrap.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-use SergiX44\Nutgram\Nutgram;
-use SergiX44\Nutgram\RunningMode\Webhook;
-use SergiX44\Nutgram\RunningMode\Polling;
-use Symfony\Contracts\Cache\ItemInterface;
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
-use Symfony\Component\Cache\Psr16Cache;
-use SergiX44\Nutgram\Configuration;
-
-
-$psr6Cache = new FilesystemAdapter();
-$psr16Cache = new Psr16Cache($psr6Cache);
-
-$bot = new Nutgram($_ENV['BOT_TOKEN'], [
-    'cache' => $psr16Cache
-]);
-
-// Running mode
-if($_ENV['ENV'] == 'development') {
-	$bot->setRunningMode(Polling::class);
-} else {
-	$bot->setRunningMode(Webhook::class);
-}
-
-require 'handler.php';
-
-$bot->registerMyCommands();
-$bot->run();
+$app->registerMyCommands();
+$app->run();
